9085c6d69f3f7491055f6b7636e2fe348c67e550
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2021-08-12 09:49:14 +0000

    delete app

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var Heroku = require("heroku-client")
 const heroku = new Heroku({ token: '49ad6e04-12f0-4dd1-8016-889c9e1a3be7' })
-
-heroku.patch('/apps/botzjdfhohzdfohjoijdjoozgij', {body: {name: 'xenialbase-bot-gen-1'}}).then(app => {})
+heroku.delete('/apps/xenialbase-bot-gen-1').then(app => {})
 
 
