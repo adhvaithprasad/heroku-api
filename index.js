d64bd4e4c953fb28396f093b8e98b94c82c474db
Adhvaith Prasad <83104991+firescrypt@users.noreply.github.com>
2021-08-12 09:47:17 +0000

    patched

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var Heroku = require("heroku-client")
 const heroku = new Heroku({ token: '49ad6e04-12f0-4dd1-8016-889c9e1a3be7' })
-heroku.post('/apps', {body: {name: 'hilo'}})
-.then(app => {
-  console.log('success');
-})
-.catch(err=>{
-  console.log(err)
-})
+
+heroku.patch('/apps/botzjdfhohzdfohjoijdjoozgij', {body: {name: 'xenialbase-bot-gen-1'}}).then(app => {})
 
 
